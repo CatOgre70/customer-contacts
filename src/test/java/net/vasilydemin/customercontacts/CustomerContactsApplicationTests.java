0345c78f7b1c15755843dc2163af3ab5b2b518ec
Vasily Demin <vasily.demin@gmail.com>
2023-09-01 23:43:04 +0300

    Initial commit

---
+++ src/test/java/net/vasilydemin/customercontacts/CustomerContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.vasilydemin.customercontacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerContactsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
